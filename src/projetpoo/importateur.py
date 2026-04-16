ff53f1206b42e047296915391898abe07d2b3016
ClemDetr <clemence.detroyat@gmail.com>
2026-04-16 21:14:51 +0200

    Update importateur.py

--- src/projetpoo/importateur.py
+++ src/projetpoo/importateur.py
@@ -1,6 +1,6 @@
 """importe la classe Texte"""
 
-class Texte:
+class Texte: # noqa : PLW1641 //the class doesn't implement the "__hash__" method
     """objet Texte"""
 
     def __init__(self, titre: str, auteur: str, contenu: str, annee: int):
@@ -8,4 +8,23 @@ class Texte:
         self._auteur = auteur
         self.contenu = contenu
 
-        self.annee = annee
+        self._annee = annee
+
+    def __str__(self) -> str:
+        return f"{self._titre} ({self._auteur}, {self._annee})"
+
+    def __repr__(self) -> str:
+        return (
+            f"Texte(titre = {self._titre!r})"
+            f"auteur = {self._auteur!r}, année = {self._annee}"
+        )
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Texte):
+            return False
+        return self._titre == other._titre and self._auteur == other._auteur
+
+    def __lt__(self, other):
+        if not isinstance(other, Texte):
+            return False
+        return self._annee < other._annee
