85b27966bb4eada4ee3fd4b6836c5fa6642597a0
Guilherme907 <guilherme.meireles567@gmail.com>
2026-04-20 15:18:48 +0200

    Create exportateur.py

---
+++ src/projetpoo/exportateur.py
@@ -0,0 +1,20 @@
+import csv
+
+Hamlet = ["Hamlet","Shakespeare"]
+Hamlet_personnages = ["Hamlet","Horatio","Ophelia","Claudius","Fantôme"]
+
+count= 0
+
+class exportateur():
+
+    with open("test.csv", "w", newline="") as csvfile:
+
+        fieldnames = ["Personnages"]
+
+        thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        thewriter.writeheader()
+
+        for personnages in Hamlet_personnages:
+            count+=1
+            thewriter.writerow({"Personnages":personnages})
