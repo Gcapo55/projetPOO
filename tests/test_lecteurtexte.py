4e99a7c5aa717aac07e3132805c9c98b3d801741
unknown <nathan.kunz@unil.ch>
2026-04-21 10:21:39 +0200

    test de la fonction découpeur de phrases

--- tests/test_lecteurtexte.py
+++ tests/test_lecteurtexte.py
@@ -5,14 +5,20 @@ import pytest
 def texte_test() -> Texte:
     t = Texte(
         "20'000 lieux sous les mers", "Jules Verne",
-        "L'année 1866 fut marquée par un événement bizarre, un phénomène "
-        "inexpliqué et inexplicable que personne n'a sans doute oublié.", 1869
+        "L'année 1866 fut marquée par un événement bizarre, un phénomène"
+        " inexpliqué et inexplicable que personne n'a sans doute oublié. Sans parler"
+        " des rumeurs qui agitaient les populations des ports et surexcitaient l'esprit"
+        " public à l'intérieur des continents les gens de mer furent particulièrement émus.", 1869
     )
     return t
 
 def test_decoupemots_fonctionne(texte_test):
     lecteur = DecoupeMots()
-    assert len(lecteur.lecture(texte_test)) == 21
+    assert len(lecteur.lecture(texte_test)) == 48
+
+def test_decoupe_phrases_fonctionne(texte_test):
+    lecteur = DecoupePhrases()
+    assert len(lecteur.lecture(texte_test)) == 2
 
 
 #test_decoupemots_fonctionne()
