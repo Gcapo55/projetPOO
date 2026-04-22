005adedd658ae1f74adf662c005a70bdad16a66a
unknown <nathan.kunz@unil.ch>
2026-04-22 15:58:04 +0200

    nouvelle fonction d'analyse adjectivale des personnages

--- src/analyse_attributs_personnages.py
+++ src/analyse_attributs_personnages.py
@@ -1,13 +1,24 @@
 ﻿from Lecteur import AnalyseTexte
+from collections import defaultdict, Counter
+
 
 class AnalyseurPersonnages:
 
-    def __init__(self, source : dict, nlp):
-        self.source = source
+    def __init__(self, dico_perso : AnalyseTexte | dict, txt_src : Doc):
+        self.dico_perso = dico_perso
+        self.txt_src = txt_src
         self.personnages = list(self.source.keys())
-    attributs_personnages = {}
+        self.attributs_personnages = defaultdict(list)
 
 
-    def dico_attributs(self) -> dict:
-        for perso in personnages :
-            pass
+    def trouver_attributs(self) -> dict:
+
+        for perso in self.personnages:
+            liste_compl = [
+                token.lemma_ for token in self.txt_src
+                if token.head.text == perso and token.dep_ == "amod"
+            ]
+            liste_red = list(Counter(liste_compl).most_common(3).keys())
+            self.attributs_personnages[perso] = liste_red
+
+        return self.attributs_personnages
