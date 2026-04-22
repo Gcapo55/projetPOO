7ad7deb6a31932fad11047d54106d411637be0b5
unknown <nathan.kunz@unil.ch>
2026-04-22 15:56:47 +0200

    adaptation des tests ; nouvelle fixture qui génère un Doc

--- tests/test_decoupeur.py
+++ tests/test_decoupeur.py
@@ -1,13 +1,13 @@
 from decoupeur import DecoupeMots, DecoupePhrases
 
-"""Tests des Découpeurs, utilisation de la fixture texte_test de conftest.py"""
+"""Tests des Découpeurs, utilisation de la fixture doc_test de conftest.py"""
 
-def test_decoupemots_fonctionne(texte_test):
+def test_decoupemots_fonctionne(doc_test):
     lecteur = DecoupeMots()
     nb_mots = 51
-    assert len(lecteur.lecture(texte_test)) == nb_mots
+    assert len(lecteur.lecture(doc_test)) == nb_mots
 
-def test_decoupe_phrases_fonctionne(texte_test):
+def test_decoupe_phrases_fonctionne(doc_test):
     lecteur = DecoupePhrases()
     nb_phrases = 2
-    assert len(lecteur.lecture(texte_test)) == nb_phrases
+    assert len(lecteur.lecture(doc_test)) == nb_phrases
