0cf49e245b3f091a3f0df929ebdc7ee416dcad33
unknown <nathan.kunz@unil.ch>
2026-04-22 11:12:33 +0200

    fichiers de pipeline et fichier d'analyse des attributs (adjectifs) des personnages

---
+++ src/analyse_attributs_personnages.py
@@ -0,0 +1,13 @@
+﻿from Lecteur import AnalyseTexte
+
+class AnalyseurPersonnages:
+
+    def __init__(self, source : dict, nlp):
+        self.source = source
+        self.personnages = list(self.source.keys())
+    attributs_personnages = {}
+
+
+    def dico_attributs(self) -> dict:
+        for perso in personnages :
+            pass
