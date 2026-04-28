74541cf5a6c82d74fe1212dcd32fcba7dabea7f6
unknown <nathan.kunz@unil.ch>
2026-04-28 16:40:34 +0200

    commentaire sur l'amélioration possible de la détection de la date de parution

--- src/importateur.py
+++ src/importateur.py
@@ -49,5 +49,5 @@ class ChargeurTexte:
             titre = re.search(r"(?<=Title:\s).+?(?=\n)", global_contenu)
             auteur = re.search(r"(?<=Author:\s).+?(?=\n)", global_contenu)
             contenu = re.search(r"\*\*\*[^*]+\*\*\*(.+?)(?=\*\*\*|$)", global_contenu, re.DOTALL)  # noqa: E501
-            date = re.search(r"(?<=Release date:\s).+?([\d]{4})(?=\s)", global_contenu)
+            date = re.search(r"(?<=Release date:\s).+?([\d]{4})(?=\s)", global_contenu)#mettre "Original Publication :"
             return Texte(titre.group(), auteur.group(), contenu.group(), date.group())
