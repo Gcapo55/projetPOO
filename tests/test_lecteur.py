5d145e5673e945ca19d9331774184c76cb3babd5
Gcapo55 <gianni.caporizzo@unil.ch>
2026-04-28 08:55:35 +0200

    Update test_lecteur.py

--- tests/test_lecteur.py
+++ tests/test_lecteur.py
@@ -1,28 +1,29 @@
 import spacy
 from lecteur import AnalyseTexte
 
-nlp = spacy.load("fr_core_news_lg")
-nlp.add_pipe("sentencizer", before="parser")
+if __name__ == "__main__":
+    nlp = spacy.load("fr_core_news_lg")
+    nlp.add_pipe("sentencizer", before="parser")
 
-texte = """
-Le 3 mai à 14h30, Napoléon quitta Paris.
-A midi, la fête commença à Lyon, mais Jacob et Michel n'étaient pas là."
-Le 21 janvier 1793 à 10:00, Louis XVI fut exécuté à Paris avec Bertrand et Sophie.
-"""
+    texte = """
+    Le 3 mai à 14h30, Napoléon quitta Paris.
+    A midi, la fête commença à Lyon, mais Jacob et Michel n'étaient pas là."
+    Le 21 janvier 1793 à 10:00, Louis XVI fut exécuté à Paris avec Bertrand et Sophie.
+    """
 
-doc = nlp(texte)
+    doc = nlp(texte)
 
-analyse = AnalyseTexte(doc)
-analyse.analyser()
+    analyse = AnalyseTexte(doc)
+    analyse.analyser()
 
-print("Personnages")
-for p in analyse.personnages.values():
-    p.afficher()
+    print("Personnages")
+    for p in analyse.personnages.values():
+        p.afficher()
 
-print("Lieux")
-for l in analyse.lieux.values():
-    l.afficher()
+    print("Lieux")
+    for l in analyse.lieux.values():
+        l.afficher()
 
-print("Événements")
-for e in analyse.evenements.values():
-    e.afficher()
+    print("Événements")
+    for e in analyse.evenements.values():
+        e.afficher()
