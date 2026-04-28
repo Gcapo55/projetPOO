867b66312dd854367a50ac264c0ece98aedff480
Gcapo55 <gianni.caporizzo@unil.ch>
2026-04-28 11:17:45 +0200

    Update lecteur
    Reconnaissances des jours de la semaine

--- src/Lecteur.py
+++ src/Lecteur.py
@@ -49,6 +49,7 @@ class AnalyseTexte:
             match_date = re.compile(
                 r"\b\d{1,2}\s+(janvier|février|mars|avril|mai|juin|juillet"
                 r"|août|septembre|octobre|novembre|décembre)(\s+\d{4})?\b"
+                r"|(?:lundi|mardi|mercredi|jeudi|vendredi|samedi|dimanche)"
                 r"|\b(en\s+)?\d{4}\b",
                 re.IGNORECASE
             ).search(sent.text)
