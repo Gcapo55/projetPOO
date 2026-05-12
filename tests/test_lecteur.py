66a8a4f2f49e6059a330dfc6c0c46e84b29e7a63
Gcapo55 <gianni.caporizzo@unil.ch>
2026-05-12 09:55:58 +0200

    Ajout test lieux

--- tests/test_lecteur.py
+++ tests/test_lecteur.py
@@ -1,8 +1,8 @@
 import pytest
 
-from corpus import Personnage
+from corpus import Personnage, Lieu
 
-
+"""Test personnages"""
 def test_nombre_personnages(dico_perso):
     """Vérifie qu'on a bien trouvé au moins un personnage."""
     assert len(dico_perso) > 0
@@ -12,10 +12,28 @@ def test_type_personnage(dico_perso):
     for perso in dico_perso:
         assert isinstance(perso, Personnage)
 
-@pytest.mark.parametrize("nom_attendu", [
-    ("Pablo"),  # Liste des noms attendus dans ton texte de test
+@pytest.mark.parametrize("perso_attendus", [
+    ("Ned Land"), ("Mr Aronnax"), # Liste des noms attendus dans ton texte de test
 ])
-def test_presence_personnage(dico_perso, nom_attendu):
+def test_presence_personnage(dico_perso, perso_attendus):
     """Vérifie qu'un personnage spécifique est présent."""
     noms = [p.nom for p in dico_perso]
-    assert nom_attendu in noms
+    assert perso_attendus in noms
+
+"""Test lieux"""
+def test_nombre_personnages(dico_lieux):
+    """Vérifie qu'on a bien trouvé au moins un lieu."""
+    assert len(dico_lieux) > 0
+
+def test_type_personnage(dico_lieux):
+    """Vérifie que ce sont bien des instances de Lieu."""
+    for perso in dico_lieux:
+        assert isinstance(perso, Lieu)
+
+@pytest.mark.parametrize("lieux_attendus", [
+    ("Paris"), # Liste des lieux attendus dans ton texte de test
+])
+def test_presence_lieux(dico_lieux, lieux_attendus):
+    """Vérifie qu'un lieu spécifique est présent."""
+    noms = [p.nom for p in dico_lieux]
+    assert lieux_attendus in noms
