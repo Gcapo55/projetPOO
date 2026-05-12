327426b6efc0bae906b79aeacf29cbfd5e3d79be
Timoteeeee <timotesarrasin@icloud.com>
2026-05-12 18:37:10 +0200

    correctif per = "Monsieur" ou autre titre

--- src/Lecteur.py
+++ src/Lecteur.py
@@ -15,6 +15,7 @@ from fonctions_evenement import (
 )
 from utils import nettoyer
 
+titres_seuls = {"monsieur", "madame", "mme", "m.", "mr"}
 
 class AnalyseTexte :
     """ Utilise spaCy pour extraire les personnages, les lieux,
@@ -95,7 +96,12 @@ class AnalyseTexte :
         """ Analyse le Doc récupéré de l'importateur en
         appelant les fonctions ajouter. """
         for ent in doc.ents:
-            if ent.label_ == "PER":
+            if ent.label_ == "PER" and not (
+                len(ent) == 1 and (
+                    "Title" in ent[0].morph.get("NameType", [])
+                    or ent[0].text.lower() in titres_seuls
+                )
+            ):
                 self._ajouter_personnage(nettoyer(ent.text), doc)
 
             elif ent.label_ in ["LOC", "GPE"]:
@@ -104,5 +110,3 @@ class AnalyseTexte :
         self.personnages = [p for p in self.personnages if p.occurrences >= min_occ]
         self.lieux = [lieu for lieu in self.lieux if lieu.occurrences >= min_occ]
         self._ajouter_events(doc)
-
-
