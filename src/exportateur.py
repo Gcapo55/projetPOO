b8793e979e248bb5c016e72cd28d4b94b88c6229
Guilherme907 <guilherme.meireles567@gmail.com>
2026-05-19 11:01:56 +0200

    Exportateur

--- src/exportateur.py
+++ src/exportateur.py
@@ -1,4 +1,5 @@
 import csv
+<<<<<<< Updated upstream
 #from Lecteur import AnalyseTexte
 
 #class Exportateur:
@@ -19,12 +20,108 @@ for i in range(len(values)):
             list.append(values[i])
             values_lists.append(list)
         counter+=1
+=======
+from corpus import Personnage,Lieu,Evenement
+from Lecteur import AnalyseTexte
+#from pathlib import Path
+>>>>>>> Stashed changes
 
 print(values_lists)
 print(fields)
 
+<<<<<<< Updated upstream
 filename="personnages.csv"
 with open(filename, "w") as csvfile:
     csvwriter=csv.writer(csvfile)
     csvwriter.writerow(fields)
     csvwriter.writerows(values_lists)
+=======
+    def __init__(self,lecteur:AnalyseTexte):
+
+        self.personnages=list[Personnage]
+        self.lieux = list[Lieu]
+        self.evenements = list[Evenement]
+
+        self.valeurs_persos=lecteur.personnages
+        self.valeurs_lieux=lecteur.lieux
+        self.valeurs_evenements=lecteur.evenements
+
+    #names=["Hamlet","Horatio","Ophelia"]
+    #values=["Hamlet","danemark","prince","Horatio","coucou","","Ophelia"]
+
+
+    def ExporterPersonnages(self):
+
+        values_lists=[]
+        fields=[]
+
+        for i in range(len(self.valeurs_persos)):
+            if self.valeurs_persos[i] in self.personnages:
+                fields.append(self.valeurs_persos[i])
+                counter=0
+            else:
+                if counter in range(len(values_lists)):
+                    values_lists[counter].append(self.valeurs_persos[i])
+                else:
+                    list=[]
+                    list.append(self.valeurs_persos[i])
+                    values_lists.append(list)
+                counter+=1
+
+        filename="personnages.csv"
+        with open(filename, "w") as csvfile:
+            csvwriter=csv.writer(csvfile)
+            csvwriter.writerow(fields)
+            csvwriter.writerows(values_lists)
+
+    def ExporterLieux(self):
+
+        values_lists=[]
+        fields=[]
+
+        for i in range(len(self.valeurs_lieux)):
+            if self.valeurs_lieux[i] in self.lieux:
+                fields.append(self.valeurs_lieux[i])
+                counter=0
+            else:
+                if counter in range(len(values_lists)):
+                    values_lists[counter].append(self.valeurs_lieux[i])
+                else:
+                    list=[]
+                    list.append(self.valeurs_lieux[i])
+                    values_lists.append(list)
+                counter+=1
+
+        filename="lieux.csv"
+        with open(filename, "w") as csvfile:
+            csvwriter=csv.writer(csvfile)
+            csvwriter.writerow(fields)
+            csvwriter.writerows(values_lists)
+
+    def ExporterEvenements(self):
+
+        values_lists=[]
+        fields=[]
+
+        for i in range(len(self.valeurs_evenements)):
+            if self.valeurs_evenements[i] in self.evenements:
+                fields.append(self.valeurs_evenements[i])
+                counter=0
+            else:
+                if counter in range(len(values_lists)):
+                    values_lists[counter].append(self.valeurs_evenements[i])
+                else:
+                    list=[]
+                    list.append(self.valeurs_evenements[i])
+                    values_lists.append(list)
+                counter+=1
+
+        filename="evenements.csv"
+        with open(filename, "w") as csvfile:
+            csvwriter=csv.writer(csvfile)
+            csvwriter.writerow(fields)
+            csvwriter.writerows(values_lists)
+
+
+
+>>>>>>> Stashed changes
---
+++ src/pipeline.py
@@ -0,0 +1,46 @@
+﻿from importateur import ChargeurTexte
+import installation_spacy # pylint: disable=unused-import
+from Lecteur import AnalyseTexte
+from exportateur import Exportateur
+from utils import spacy_conv, patienter
+
+
+class Pipeline:
+    """Classe qui fait fonctionner toute l'architecture
+    du projet (import, analyse, export)"""
+    def __init__(self,
+                 source : str,
+                 chargeur : ChargeurTexte,
+                 finder : AnalyseTexte,
+                 exportateur : Exportateur,
+                 ):
+        self.source = source
+        self._chargeur = chargeur
+        self._finder = finder
+        self._exportateur = exportateur
+
+    def executer(self):
+        """Fonction qui fait l'exécution du programme"""
+        patienter()
+        texte = self._chargeur.charger(self.source)
+        doc = spacy_conv(texte)
+        self._finder.analyser(doc, 10)
+        #liste_perso = self._finder.personnages
+        #liste_lieu = self._finder.lieux
+        #liste_evenements = self._finder.evenements
+        self._exportateur.ExporterPersonnages()
+        self._exportateur.ExporterLieux()
+        self._exportateur.ExporterEvenements()
+        #print(liste_perso)
+        #print(liste_lieu)
+        #print(*liste_evenements, sep="\n")
+
+
+if __name__ == "__main__" :
+
+    pipeline = Pipeline("20'000 lieux sous les mers.txt",
+                    ChargeurTexte(),
+                    AnalyseTexte(),
+                    )
+
+    pipeline.executer()
